abdd9daf61fc9f80c60ddb35db4fa4bbbfb09fc4
Mark Du <dulongjiebackup@gmail.com>
2023-07-28 14:19:17 -0400

    add findByTourPackageCode method in TourRepository

--- src/main/java/com/spring/spring_ms/data/repositories/TourRepository.java
+++ src/main/java/com/spring/spring_ms/data/repositories/TourRepository.java
@@ -3,5 +3,8 @@ package com.spring.spring_ms.data.repositories;
 import com.spring.spring_ms.data.entities.Tour;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TourRepository extends CrudRepository<Tour, Integer> {
+    List<Tour> findByTourPackageCode(String code);
 }
